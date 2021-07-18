46cb41b978621dce660eb8f51f4f0485c7f220e5
mcmodder2021 <87586886+mcmodder2021@users.noreply.github.com>
2021-07-18 19:15:44 -0400

    This is just a basic addon
    This is for cookie clicker

--- addon.js
+++ addon.js
@@ -1,1 +1,9 @@
-var autoclicker = setInterval(function() { Game.ClickCookie(); }, 10);
+var Addon = setInterval(function(){ 
+  for (var h in Game.shimmers){
+    if(Game.shimmers[h].type=="golden"){
+      Game.shimmers[h].pop();}}for (var h in Game.shimmers){if(Game.shimmers[h].type=="wrath"){
+        Game.shimmers[h].pop();}} 
+  try{
+    document.getElementById('bigCookie').click();
+  } catch (err) { clearInterval(); } }, 100
+);                      
